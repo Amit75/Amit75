f7a5b8e23eaa0b54ae46fd296fa71de927d69722
Amit75 <60569752+Amit75@users.noreply.github.com>
2026-08-23 17:41:06 +0530

    Remove obsolete Android Kotlin plugin

--- aarulya-app-store/android-store/app/build.gradle.kts
+++ aarulya-app-store/android-store/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 val trustedReleaseKeyFingerprints = providers.gradleProperty("aarulyaReleaseKeyFingerprints").orElse("")
